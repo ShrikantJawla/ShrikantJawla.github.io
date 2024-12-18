76ea9c8054a86946729e3a7fb29b538ae2127e04
ShrikantJawla <shrikantjawla@gmail.com>
2024-12-19 00:44:27 +0530

    updated

--- src/app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
-
-export const GET = (req: NextRequest) => {
-
-  return NextResponse.json({
-    result: "ok",
-  });
-};
