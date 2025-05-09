9297e7ad146fad1977bfff2415826de5e580f5c2
ShrikantJawla <shrikantjawla@gmail.com>
2025-05-09 15:16:41 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true, // Disable image optimization for static export
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
